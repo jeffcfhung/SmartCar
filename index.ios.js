b099d16bea6609800a5c3e8d159fe2a8c82cd6a0
Jeff Hung <jhung@ruckuswireless.com>
2017-08-31 20:37:59 -0700

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React from 'react-native';
+import Viewport from './app/Viewport'
+
+React.AppRegistry.registerComponent('SmartCar', () => Viewport)
+//AppRegistry.registerComponent('SmartCar', () => SmartCar);
